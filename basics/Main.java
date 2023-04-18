47a8732cf38818db916c084a845c8fe60e1b66b6
DaveyAuz <doswald27@gmail.com>
2023-04-18 00:38:56 -0400

    tried my best at the lab

--- basics/Main.java
+++ basics/Main.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
@@ -22,22 +23,46 @@ public class Main {
 
 public static String pluralize(String word, int count) {
     if (count == 0 || count > 1) {
+        int dogCount = 1;
+        System.out.println("I own " + dogCount + " " + pluralize("dog", dogCount) + ".");
+        
+        int catCount = 2;
+        System.out.println("I own " + catCount + " " + pluralize("cat", catCount) + ".");
+        
+        int turtleCount = 0;
+        System.out.println("I own " + turtleCount + " " + pluralize("turtle", turtleCount) + ".");
         return word + "s";
     }
     return word;
+}
 
-    int dogCount = 1;
-    System.out.println("I own " + dogCount + " " + pluralize("dog", dogCount) + ".");
-    
-    int catCount = 2;
-    System.out.println("I own " + catCount + " " + pluralize("cat", catCount) + ".");
-    
-    int turtleCount = 0;
-    System.out.println("I own " + turtleCount + " " + pluralize("turtle", turtleCount) + ".");
+
+
+
+    public static void flipNHeads(int n) {
+        Random rand = new Random();
+        int flips = 0;
+        int heads_in_a_row = 0;
+
+        while (heads_in_a_row < n) {
+            double flip = rand.nextDouble();
+            flips++;
+
+            if (flip < 0.5) {
+                System.out.println("tails");
+                heads_in_a_row = 0;
+            } else {
+                System.out.println("heads");
+                heads_in_a_row++;
+            }
+        }
+
+        System.out.printf("It took" + flips + " flips to flip " + n + "heads in a row.");
+    }
 
 }
 
-}
+
 
 
 
