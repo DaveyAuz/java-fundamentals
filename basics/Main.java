434ffde73f2edf7174c2970e8cb2158ae22c098b
DAVEY J OSWALD <Doswald27@gmail.com>
2023-04-19 01:36:48 -0400

    finished lab 1

--- basics/Main.java
+++ basics/Main.java
@@ -1,10 +1,59 @@
 import java.util.Random;
+import java.time.LocalDateTime;
 
 public class Main {
     public static void main(String[] args) {
-    String greeting = "Welcome to my Java program.";
-    System.out.println(greeting);
+        System.out.println(getGreeting());
+        pluralize("cat", 1);
+        pluralize("cat", 2);
+        flipNHeads(2);
+        clock();
+    }
+
+    public static String getGreeting() {
+        return "Hello World!";
+    }
+    //
+    public static void pluralize(String word, int count) {
+        if (count == 0 || count > 1) {
+            System.out.println(word + "s");
+        } else {
+            System.out.println(word);
+        }
+    }
+    //
+    public static void flipNHeads(int n) {
+        int count = 0;
+        int heads = 0;
+        while (heads < n) {
+            double random = Math.random();
+            if (random < 0.5) {
+                System.out.println("tails");
+                heads = 0;
+            } else {
+                System.out.println("heads");
+                heads++;
+            }
+            count++;
+        }
+        System.out.println("It took " + count + " flips to flip " + n + " heads in a row.");
+    }
+    //
+    public static void clock() {
+        int seconds = 0;
+        while (true) {
+            LocalDateTime now = LocalDateTime.now();
+            int hour = now.getHour();
+            int minute = now.getMinute();
+            int second = now.getSecond();
+            if (second != seconds) {
+                seconds = second;
+                System.out.println(hour + ":" + minute + ":" + second);
+            }
+        }
+    }
 }
+
 
 
 // int dogCount = 1;
@@ -18,51 +67,6 @@ public class Main {
 // I own 1 dog.
 // I own 2 cats.
 // I own 0 turtles.
-
-
-
-public static String pluralize(String word, int count) {
-    if (count == 0 || count > 1) {
-        int dogCount = 1;
-        System.out.println("I own " + dogCount + " " + pluralize("dog", dogCount) + ".");
-        
-        int catCount = 2;
-        System.out.println("I own " + catCount + " " + pluralize("cat", catCount) + ".");
-        
-        int turtleCount = 0;
-        System.out.println("I own " + turtleCount + " " + pluralize("turtle", turtleCount) + ".");
-        return word + "s";
-    }
-    return word;
-}
-
-
-
-
-    public static void flipNHeads(int n) {
-        Random rand = new Random();
-        int flips = 0;
-        int heads_in_a_row = 0;
-
-        while (heads_in_a_row < n) {
-            double flip = rand.nextDouble();
-            flips++;
-
-            if (flip < 0.5) {
-                System.out.println("tails");
-                heads_in_a_row = 0;
-            } else {
-                System.out.println("heads");
-                heads_in_a_row++;
-            }
-        }
-
-        System.out.printf("It took" + flips + " flips to flip " + n + "heads in a row.");
-    }
-
-}
-
-
 
 
 
